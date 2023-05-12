3b86ecf30aa1eba294b911ee34b33ec4e0d1113f
Alexey Jukov <lexagukan@mail.ru>
2023-05-12 10:58:12 +0300

    fix: Свайпер на мобиле работает
    Осталось отключить свайпер не на мобиле, при большем разрешении

--- scripts/swiper.js
+++ scripts/swiper.js
@@ -1,9 +1,15 @@
 
-  const swiper = new Swiper('.swiper', {
+  const swiper = new Swiper('.swiper-object .swiper', {
     direction: 'horizontal',
     loop: true,
+    slidesPerView: 1,
+    spaceBetween: 16,
+    loopedSlides: 2,
+    
     pagination: {
       el: '.swiper-pagination',
+      clickable: true,
+      
     },
   });
 
