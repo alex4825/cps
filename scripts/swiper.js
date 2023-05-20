18097cee1dda0de1d78e3e214d18675c300a955d
Alexey Jukov <lexagukan@mail.ru>
2023-05-20 07:25:01 +0300

    fix: Попытка исправления свайпера для мобилок
    До этого в js проверялась ширина окна браузера для включения/отключения свайпера. Сейчас проверяется  именно устройство, с которого зашли на сайт

--- scripts/swiper.js
+++ scripts/swiper.js
@@ -1,29 +1,36 @@
-const swiper = new Swiper('.swiper-object .swiper', {
-  direction: 'horizontal',
-  slidesPerView: 1,
-  spaceBetween: 16,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,   
-  },  
-});
+function isMobile(){
+  return (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent));
+}
+var swiper;
+if(isMobile()){
+  swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    slidesPerView: 1,
+    spaceBetween: 16,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,   
+    },  
+  });
 
-window.addEventListener('resize', function(event) {
-  changeSwiper();  
-}, true);
-
-window.addEventListener('load', function(event) {
-  changeSwiper();
-}, true);
-
-function changeSwiper(){
-  if(window.innerWidth > 768){    
-    swiper.slideTo(0, 500);
-    swiper.update();
-    swiper.enabled = false;
-  }
-  else{
-    swiper.enabled = true;
-    swiper.update();
+  window.addEventListener('resize', function(event) {
+    changeSwiper();  
+  }, true);
+  
+  window.addEventListener('load', function(event) {
+    changeSwiper();
+  }, true);
+  
+  function changeSwiper(){  
+    if(window.innerWidth > 768 && !isMobile()){    
+      swiper.slideTo(0, 500);
+      swiper.update();
+      swiper.enabled = false;
+    }
+    else{
+      swiper.enabled = true;
+      swiper.update();
+    }
   }
 }
+
