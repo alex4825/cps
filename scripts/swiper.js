c48654e724bae2f06857e992b14de62e8c824ebd
Alexey Jukov <lexagukan@mail.ru>
2023-05-20 08:09:25 +0300

    fix: Теперь всё работает исправно
    Свайпер инициализируется в любом случае и работает как на мобиле, так и при ресайзе на разрешение менее 768px

--- scripts/swiper.js
+++ scripts/swiper.js
@@ -1,36 +1,33 @@
 function isMobile(){
-  return (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent));
+  return (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))
+  || window.innerWidth < 768;
 }
 var swiper;
-if(isMobile()){
-  swiper = new Swiper('.swiper', {
-    direction: 'horizontal',
-    slidesPerView: 1,
-    spaceBetween: 16,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,   
-    },  
-  });
+swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  slidesPerView: 1,
+  spaceBetween: 16,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,   
+  },  
+});
+window.addEventListener('resize', function(event) {
+  changeSwiper();  
+}, true);
 
-  window.addEventListener('resize', function(event) {
-    changeSwiper();  
-  }, true);
-  
-  window.addEventListener('load', function(event) {
-    changeSwiper();
-  }, true);
-  
-  function changeSwiper(){  
-    if(window.innerWidth > 768 && !isMobile()){    
-      swiper.slideTo(0, 500);
-      swiper.update();
-      swiper.enabled = false;
-    }
-    else{
-      swiper.enabled = true;
-      swiper.update();
-    }
+window.addEventListener('load', function(event) {
+  changeSwiper();
+}, true);
+
+function changeSwiper(){  
+  if(!isMobile()){    
+    swiper.slideTo(0, 500);
+    swiper.update();
+    swiper.enabled = false;
+  }
+  else{
+    swiper.enabled = true;
+    swiper.update();
   }
 }
-
